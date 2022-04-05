6b15e44f0a78758c951bca31771a5028e6456e7e
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2022-04-05 09:39:08 +0200

    Add project files.

---
+++ OOP___Chess/Figure.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP___Chess
+{
+    public class Figure
+    {
+        private List<string> numbers = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8" };
+        private List<string> letters = new List<string>() { "A", "B", "C", "D", "F", "G", "E", "H" };
+        private string position;
+
+        public FigureType Type { get; }
+        public string Position
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value)) // hodnota v proměnné value je null nebo ""
+                {
+                    position = value; //figura je mimo šachovnici
+                }
+                else if (value.Length == 2)
+                {    //první znak v proměnné value je v listu letters a druhý znak je v listu numbers
+                    if (letters.Contains(value.Substring(0, 1).ToUpper()) && numbers.Contains(value.Substring(1, 1))) // druhý znak v proměnné value je v listu numbers
+                    {
+                        position = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Figure position value has to be valid chess position!");
+                    }
+                }
+
+                else
+                {
+                    throw new Exception("Figure position value has to be 2 letters valid chess position!");
+                }
+            }
+            get => position;
+        }
+        public FigureColor Color { get ;}
+
+        public Figure(FigureType type, FigureColor color)
+        {
+            Type = type;
+            Color = color;
+        }
+
+        public Figure(FigureType type, string position, FigureColor color)
+        {
+            Type = type;
+            Position = position;
+            Color = color;
+        }
+    }
+
+    // výčtový datový typ - seznam hodnot - interně jsou to hodnoty integer (od 0)-
+    public enum FigureType
+    {
+        Pawn,       // pěšec
+        Rook,       // věž 
+        Knight,     // jezdec 
+        Bishop,     // střelec 
+        Queen,      // dáma 
+        King,       // král
+    }
+
+    public enum FigureColor
+    {
+        White,
+        Black
+    }
+}
+
+    
+
---
+++ OOP___Chess/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP___Chess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Figure> figures;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            figures.Add(new Figure(FigureType.Rook, "A8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Rook, "H8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Knight, "B8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Knight, "G8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Bishop, "C8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Bishop, "F8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Queen, "D8", FigureColor.Black));
+            figures.Add(new Figure(FigureType.King, "E8", FigureColor.Black));
+
+            figures.Add(new Figure(FigureType.Pawn, "A7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "H7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "B7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "G7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "C7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "F7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "D7", FigureColor.Black));
+            figures.Add(new Figure(FigureType.Pawn, "E7", FigureColor.Black));
+
+            figures.Add(new Figure(FigureType.Rook, "A1", FigureColor.White));
+            figures.Add(new Figure(FigureType.Rook, "H1", FigureColor.White));
+            figures.Add(new Figure(FigureType.Knight, "B1", FigureColor.White));
+            figures.Add(new Figure(FigureType.Knight, "G1", FigureColor.White));
+            figures.Add(new Figure(FigureType.Bishop, "C1", FigureColor.White));
+            figures.Add(new Figure(FigureType.Bishop, "F1", FigureColor.White));
+            figures.Add(new Figure(FigureType.Queen, "D1", FigureColor.White));
+            figures.Add(new Figure(FigureType.King, "E1", FigureColor.White));
+
+            figures.Add(new Figure(FigureType.Pawn, "A2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "H2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "B2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "G2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "C2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "F2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "D2", FigureColor.White));
+            figures.Add(new Figure(FigureType.Pawn, "E2", FigureColor.White));
+        }
+    }
+}
